85767a47296f7db04b7412b7baaa5f4dfbcb1c04
Krish Suchak <suchak.krish@gmail.com>
2025-05-04 19:35:33 -0400

    start keepalive script

---
+++ util/keepalive.py
@@ -0,0 +1,2 @@
+# workflow_files: "api.yml, deps.yml, dividends.yml, indicators.yml, indices.yml, intraday.yml, model.yml, ohlc.yml, optimize.yml, predict.yml, splits.yml, symbols.yml, unrate.yml, visualize.yml"
+from github import Github
