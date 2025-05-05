9563661eed9d074cd022762fd0e95ce392373eb2
Krish Suchak <suchak.krish@gmail.com>
2025-05-04 20:14:59 -0400

    almost done

--- util/keepalive.py
+++ util/keepalive.py
@@ -1,2 +1,18 @@
-# workflow_files: "api.yml, deps.yml, dividends.yml, indicators.yml, indices.yml, intraday.yml, model.yml, ohlc.yml, optimize.yml, predict.yml, splits.yml, symbols.yml, unrate.yml, visualize.yml"
-from github import Github
+import os
+from dotenv import load_dotenv, find_dotenv
+from github import Github, Auth
+
+load_dotenv(find_dotenv('config.env'))
+
+token = os.environ.get('GITHUB')
+repo_name = os.environ.get('GITHUB_REPOSITORY')
+auth = Auth.Token(token)
+git = Github(auth=auth)
+repo = git.get_repo(repo_name)
+workflows = repo.get_workflows()
+for workflow in workflows:
+    try:
+        workflow.enable()
+    except Exception as e:
+        print(e)
+git.close()
