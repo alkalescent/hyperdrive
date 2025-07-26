be8f0a462b67ba29a7e8606453f52954825d6042
Krish Suchak <suchak.krish@gmail.com>
2025-07-25 20:38:59 -0400

    add class comment

--- hyperdrive/Crypt.py
+++ hyperdrive/Crypt.py
@@ -1,16 +1,25 @@
+import os
 import base64
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.scrypt import Scrypt
 
 
 class Cryptographer:
+    """
+    A class for symmetric encryption using AES-256 in GCM mode.
+
+    The key is derived from a user-provided password and salt using Scrypt.
+    This approach is considered post-quantum
+    resistant for symmetric encryption.
+    """
+
     def __init__(self, password, salt):
         kdf = Scrypt(
             salt=salt,
             length=32,
-            n=2**14,
+            n=2**16,
             r=8,
-            p=1,
+            p=2,
         )
         key = base64.urlsafe_b64encode(kdf.derive(password))
         self.f = Fernet(key)
