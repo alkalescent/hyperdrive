f559eda0129105eb6c347cf4442aeaab8796197d
Krish Suchak <suchak.krish@gmail.com>
2025-07-25 21:47:57 -0400

    tests

--- test/test_Crypt.py
+++ test/test_Crypt.py
@@ -3,29 +3,28 @@ sys.path.append('hyperdrive')
 from Crypt import Cryptographer  # noqa autopep8
 
 
-password = b'securepassword'
+password = b'password'
 salt = b'salt'
+crypt = Cryptographer(password, salt)
 
 
 class TestCryptographer():
     def test_encrypt_decrypt(self):
-        cryptographer = Cryptographer(password, salt)
-
         plaintext = b'This is a secret message.'
-        ciphertext = cryptographer.encrypt(plaintext)
+        ciphertext = crypt.encrypt(plaintext)
 
-        decrypted_text = cryptographer.decrypt(ciphertext)
+        decrypted_text = crypt.decrypt(ciphertext)
 
-        assert decrypted_text == plaintext, "Decrypted text does not match original plaintext."
+        assert decrypted_text == plaintext, (
+            "Decrypted text does not match original plaintext.")
 
     def test_invalid_decryption(self):
-        cryptographer = Cryptographer(password, salt)
-
         invalid_ciphertext = b'invaliddata'
 
         try:
-            cryptographer.decrypt(invalid_ciphertext)
-            assert False, "Expected decryption to fail with invalid ciphertext."
+            crypt.decrypt(invalid_ciphertext)
+            assert False, (
+                "Expected decryption to fail with invalid ciphertext.")
         except Exception as e:
             assert isinstance(
                 e, Exception), "Expected an exception for invalid decryption."
