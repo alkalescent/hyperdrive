ff42238dd594bf0fcc9c7a529529c48a1bd967eb
Krish Suchak <suchak.krish@gmail.com>
2025-07-29 01:16:41 -0400

    ndx update

--- scripts/update_ohlc.py
+++ scripts/update_ohlc.py
@@ -11,7 +11,7 @@ alpc = AlpacaData(paper=C.TEST)
 poly = Polygon(os.environ['POLYGON'])
 stock_symbols = poly.get_symbols()
 poly_symbols = stock_symbols + C.POLY_CRYPTO_SYMBOLS
-alpc_symbols = set(alpc.get_ndx()[C.SYMBOL]).union(stock_symbols)
+alpc_symbols = set(alpc.get_saved_ndx()[C.SYMBOL]).union(stock_symbols)
 
 
 def update_poly_ohlc():
@@ -36,7 +36,7 @@ def update_alpc_ohlc():
     for symbol in alpc_symbols:
         try:
             filename = alpc.save_ohlc(
-                symbol=symbol, timeframe=C.FEW_DAYS, retries=1)
+                symbol=symbol, timeframe='6y', retries=1)
             with counter.get_lock():
                 counter.value += 1
         except Exception as e:
@@ -49,12 +49,13 @@ def update_alpc_ohlc():
                 os.remove(filename)
 
 
-p1 = Process(target=update_poly_ohlc)
-p2 = Process(target=update_alpc_ohlc)
-p1.start()
-p2.start()
-p1.join()
-p2.join()
+if __name__ == "__main__":
+    # p1 = Process(target=update_poly_ohlc)
+    p2 = Process(target=update_alpc_ohlc)
+    # p1.start()
+    p2.start()
+    # p1.join()
+    p2.join()
 
-if counter.value / (len(poly_symbols) + len(alpc_symbols)) < 0.95:
-    exit(1)
+    if counter.value / (len(poly_symbols) + len(alpc_symbols)) < 0.95:
+        exit(1)
