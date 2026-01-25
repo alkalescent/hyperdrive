b47bc4408facaad84e88d4fff30eaab93b6b4ab8
Krish Suchak <suchak.krish@gmail.com>
2026-01-24 20:50:41 -0500

    start spreadsheet

--- scripts/update_spreadsheet.py
+++ scripts/update_spreadsheet.py
@@ -1,7 +1,17 @@
+import gspread
 import polars as pl
+import pandas as pd
 from hyperdrive.Broker import Robinhood
 
-rh = Robinhood()
-dividends = rh.get_dividends()
-df = pl.from_dicts(dividends)
+gc = gspread.service_account()
+sh = gc.open("FIRE").get_worksheet(0)
+records = sh.get_all_records()
+# df = pl.DataFrame(records[:-755])
+# print(records)
+df = pl.from_pandas(pd.DataFrame(records))
+print(df)
+
+# rh = Robinhood()
+# dividends = rh.get_dividends()
+# df = pl.from_dicts(dividends)
 
