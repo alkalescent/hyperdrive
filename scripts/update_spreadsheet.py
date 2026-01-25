1b768de32a0df1fbb6e8462e80b8b7dbf6686736
Krish Suchak <suchak.krish@gmail.com>
2026-01-24 21:15:05 -0500

    only relevant cols

--- scripts/update_spreadsheet.py
+++ scripts/update_spreadsheet.py
@@ -1,15 +1,20 @@
 import gspread
-import polars as pl
 import pandas as pd
 from hyperdrive.Broker import Robinhood
 
 gc = gspread.service_account()
 sh = gc.open("FIRE").get_worksheet(0)
 records = sh.get_all_records()
-# df = pl.DataFrame(records[:-755])
+df = pd.DataFrame(records)
+cols = list(df.columns)
+total_idx = cols.index("Total")
 # print(records)
-df = pl.from_pandas(pd.DataFrame(records))
-print(df)
+# df = pl.from_pandas(pd.DataFrame(records))
+print(df[cols[:total_idx]])
+
+
+
+# print(list(df.columns).index("Total"))
 
 # rh = Robinhood()
 # dividends = rh.get_dividends()
